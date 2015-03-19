f61b1ae568cc4f70b8a0ca17fdbf38e475c36b36
Larry01 <b03902108@ntu.edu.tw>
2015-03-19 17:53:16 +0800

    modify 002.gcd

--- Algorithm/002.gcd.cpp
+++ Algorithm/002.gcd.cpp
@@ -5,11 +5,18 @@ int gcd(int a,int b){
 	return a==0 ? b:gcd(b%a,a);
 }
 
+
+int gcd2(int a,int b){
+	return a ? gcd(b%a, a) : b ;
+}
+
+
 int main(){
 	int x,y;
 	cout<<"Please input two integer :";
 	cin>>x>>y;
 	cout<<gcd(x,y)<<endl;
+	cout<<gcd2(x,y)<<endl;
 	
 	return 0;
 }
