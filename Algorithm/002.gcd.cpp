b219d0b93d185f11eb4d1360be7890550ee8e115
JRecode <watermelo0326@gmail.com>
2015-03-19 22:55:01 +0800

    modify to gcd <part of return>

--- Algorithm/002.gcd.cpp
+++ Algorithm/002.gcd.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 int gcd(int a,int b){
-	return a==0 ? b:gcd(b%a,a);
-}
-
-
-int gcd2(int a,int b){
 	return a ? gcd(b%a, a) : b ;
 }
 
@@ -16,7 +11,6 @@ int main(){
 	cout<<"Please input two integer :";
 	cin>>x>>y;
 	cout<<gcd(x,y)<<endl;
-	cout<<gcd2(x,y)<<endl;
 	
 	return 0;
 }
