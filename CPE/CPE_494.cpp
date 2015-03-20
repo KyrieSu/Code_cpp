dd2c8d49616594f6b8238f7162e211dd80878fd6
JRecode <watermelo0326@gmail.com>
2015-03-21 02:18:05 +0800

    modify the ultimate loop

--- CPE/CPE_494.cpp
+++ CPE/CPE_494.cpp
@@ -3,20 +3,29 @@
 #include<cstdio>
 using namespace std;
 
-char str[200],*ptr,input[100][30];
+char str[200],*ptr;
 int num,array[100];
 
 int main(){
-	cout<<"mayday~ I can't so the ultimate loop.....But my test is great '"<<endl;
-	int keyin;
+	cout<<"Enter 0 to end program"<<endl;
+	int keyin,i;
+	for(i=0;i>=0;i++){
 	num=0;
 	gets(str);
+	if(str[0]=='0')
+	 break;
 	ptr = strtok(str," ") ;
 	while(ptr != NULL){
 		num++;
        	ptr = strtok(NULL," ") ;
     }
-   	cout<<num<<endl;
+    array[i]=num;   
+	}
+	
+	for(int j=0;j<i;j++)
+		cout<<endl<<array[j]<<endl;
+	
+   	
 	
 	
 	return 0;
