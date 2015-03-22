2d6ecd2b0b6031d0ac0bff801d1fe54fc22c0a68
JRecode <watermelo0326@gmail.com>
2015-03-22 15:31:12 +0800

    add isalpha() to check

--- CPE/CPE_494_ver2.cpp
+++ CPE/CPE_494_ver2.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstring>
 #include<cstdio>
+#include<ctype.h>
 using namespace std;
 
 char str[200],*ptr;
@@ -10,16 +11,17 @@ int main(){
 	cout<<"Enter 0 to end program"<<endl;
 	int keyin,i;
 	for(i=0;i>=0;i++){
-	num=0;
-	gets(str);
-	if(str[0]=='0')
-	 break;
-	ptr = strtok(str," ") ;
-	while(ptr != NULL){
-		num++;
-       	ptr = strtok(NULL," ") ;
-    }
-    array[i]=num;   
+		num=0;
+		gets(str);
+		if(str[0]=='0')
+	 		break;
+		ptr = strtok(str," ") ;
+		while(ptr != NULL){
+			if(isalpha(*ptr)) 
+				num++;
+       		ptr = strtok(NULL," ") ;
+    	}
+    	array[i]=num;   
 	}
 	
 	for(int j=0;j<i;j++)
