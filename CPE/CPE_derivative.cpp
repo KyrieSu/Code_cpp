9f85676dad17513043839b9e52814ec33961415c
JRecode <watermelo0326@gmail.com>
2015-03-23 00:14:50 +0800

    use link list to store data

---
+++ CPE/CPE_derivative.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+#include<cmath>
+using namespace std;
+
+struct data{
+	int x;
+	int cef[50]; /* a0,a1,....,an */
+	int pow[50]; 
+};
+
+data *head,*now;
+
+int main(){
+	cout<<"Derivative the Polly polynomial"<<endl;
+	for(int i=0;i>=0;i++){
+		
+	}
+	
+	return 0;
+}
