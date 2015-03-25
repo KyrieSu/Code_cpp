fb9b2ce10c218fbbad555cffe5e48026c956786b
JRecode <watermelo0326@gmail.com>
2015-03-25 21:57:45 +0800

    Add the ultimate loop

--- CPE/CPE_001.cpp
+++ CPE/CPE_001.cpp
@@ -1,21 +1,33 @@
 #include<iostream>
 using namespace std;
 
+
 int main(){
-	int num,sum=0,avg,step=0;
+	int num,total=0,step[100]={0}; 
+	while(true){
+	int sum=0;
 	cin>>num;
+	if(num==0)
+		break;	
 	int *arr=new int [num];
 	for(int i=0;i<num;i++){
 		cin>>arr[i];
 		sum+=arr[i];
 	}
-	avg=sum/num;
+	int avg=sum/num; /* The index of Step[] */
 	for(int i=0;i<num;i++){
 		if(arr[i]>avg)
-			step+=(arr[i]-avg);
+			step[total]+=(arr[i]-avg);
 	}
-	cout<<"The minimum step of moves is "<<step<<"."<<endl;
 	delete [] arr;
+	total++;
+	}
+	
+	cout<<endl;
+	for(int i=0;i<total;i++){
+		cout<<"Set #"<<i+1<<endl;
+		cout<<"The minimun step of moves is "<<step[i]<<endl<<endl;
+	}
 	
 	return 0;
 }
