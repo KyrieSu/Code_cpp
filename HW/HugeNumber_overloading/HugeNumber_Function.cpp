fb7fbef46beefbf4f62170f854aad8ac9fad8fa2
JRecode <watermelo0326@gmail.com>
2015-04-12 23:10:28 +0800

    divison_function

--- HW/HugeNumber_overloading/HugeNumber_Function.cpp
+++ HW/HugeNumber_overloading/HugeNumber_Function.cpp
@@ -1,23 +1,24 @@
 #include<iostream>
 #include"HugeNumber.h"
+#include<vector>
 using namespace std;
 
 HugeNumber::HugeNumber(){
 	check = true;
-	my_digit = 0;
-	/* I think digit might be 1 not 0 */
+	my_digit = 1;
 	for (int i = 0; i < Max; i++)
 		data[i] = 0;
 }
 
 ostream &operator<<(ostream &out, const HugeNumber &toput){
 	if (!toput.check){
-		cout /* cout ??????? */<< "(negative)0" << endl;
+		cout << "(negative)0" << endl;
 		return;
 	}
 	for (int i = 0; i < my_digit; i++)
 		out << data[i];
-	/* endl or not */
+	out << endl;
+
 	return out;
 }
 
@@ -52,15 +53,14 @@ bool HugeNumber::operator!=(const HugeNumber &right){
 bool HugeNumber::operator>(const HugeNumber &right){
 	if (this->my_digit < right.my_digit)
 		return false;
-	for (int i = my_digit - 1; i >= 0;i++ /* i++ ???????? */)
+	for (int i = my_digit - 1; i >= 0;i--)
 		if (this->data[i] < right.data[i])
 			return false;
 	return true;
 }
 
 bool HugeNumber::operator<(const HugeNumber &right){
-	return !(*this>right);
-	/* !(>) is <= not < */
+	return ! ( (*this>right) && (*this==right) );
 }
 
 HugeNumber HugeNumber::operator+(const HugeNumber &addin){
@@ -84,33 +84,24 @@ HugeNumber HugeNumber::operator+(const HugeNumber &addin){
 
 HugeNumber HugeNumber::operator-(const HugeNumber &right){
 	HugeNumber num;
-	if (my_digit < right.my_digit){
-		num.check = false;
-		return;
-	}
-
-	/* The exceptional situation */
-	if (my_digit == 1 && right.my_digit == 1 && data[0] == right.data[0]){
-		num.check = true;
-		num.data[0] = 0;
-		return;
-	}
-
-	for (int i = 0; i < my_digit; i++)
+	num.check = (this->my_digit >= right.my_digit);
+	if (!num.check) return;
+	for (int i = 0; i < num.my_digit; i++){
 		num.data[i] = this->data[i] - right.data[i];
-	for (int i = 0; i < num.my_digit-1; i++)
 		if (num.data[i] < 0){
-			num.data[i + 1] -= 1;
+			num.data[i + 1]--;
 			num.data[i] += 10;
 		}
-
-	while (num.data[num.my_digit - 1] <= 0){
-		if (num.data[num.my_digit - 1] < 0){
-			num.check = false;
-			return;
-		}
-		num.my_digit--;
 	}
+
+
+	if (num.data[my_digit] < 0){
+		check = false;
+		return;
+	}
+
+	while (data[my_digit - 1] == 0 && my_digit != 1)
+		my_digit--;
 
 	return num;
 }
@@ -119,21 +110,24 @@ HugeNumber HugeNumber::operator*(const HugeNumber &right){
 	HugeNumber num;
 	num.my_digit = this->my_digit + right.my_digit; //the max digit 
 	for (int i = 0; i < this->my_digit; i++)
-		for (int j = 0,k=i; j < right.my_digit; j++,k++)
-			num.data[k] += this->data[i] * right.data[j];
-
-	/*
-	for(int i=0 ; i<this->my_digit ; i++)
-		for(int j=0 ; j<right.my_digit ; j++)
-			num.data[i+j] += this.data[i] * this.data[j] ;
-	*/
-
+		for (int j = 0; j < right.my_digit; j++)
+			num.data[i+j] += this->data[i] * right.data[j];
 	for (int i = 0; i < num.my_digit; i++){
-		num.data[i + 1] = num.data[i]/10;
+		num.data[i + 1] += num.data[i]/10;
 		num.data[i] %= 10;		
 	}
 	while (!num.data[my_digit - 1])
 		my_digit--;
+
+	return num;
+}
+
+HugeNumber HugeNumber::operator/(const HugeNumber &right){
+	HugeNumber num;
+	if (right.my_digit == 1 && right.data[0] == 0)
+		return;
+	num.my_digit = this->my_digit - right.my_digit + 1;
+	
 
 	return num;
 }
