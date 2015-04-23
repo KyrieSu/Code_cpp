c84ec482f674d643966ada3e2cc3c850aac48c9e
JRecode <watermelo0326@gmail.com>
2015-04-23 10:44:17 +0800

    funny problem

---
+++ CPE/happy.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+/* https://leetcode.com/problems/happy-number/ */
+/* [2,6] is not happy number*/
+
+bool happy(int num){
+	while(num>6){
+		int sum=0;
+		while(num){
+			sum+=(num%10)*(num%10);
+			num/=10;
+		}
+	num=sum;
+	}
+	return num==1;
+}
+
+
+int main(){
+	int number;
+	cin>>number;
+	if(happy(number))
+		cout<<number<<" is a happy number"<<endl;
+	else
+		cout<<number<<" is not a happy number"<<endl;	
+}
