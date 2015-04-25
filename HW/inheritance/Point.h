606e22bf5a8efd7670ac96b64b38c6a6bdec40b7
JRecode <watermelo0326@gmail.com>
2015-04-25 15:28:26 +0800

    inheritance HW

---
+++ HW/inheritance/Point.h
@@ -0,0 +1,19 @@
+#ifndef Point_h
+#define Point_h
+
+class Point{
+public:
+	Point(int =0,int =0);
+private:
+	int x;
+	int y;
+};
+
+class Figure{
+public:
+	void area(int,int,int,int);
+};
+
+
+
+#endif
