3dd7753889247b6b9bf866441756451ea5637d73
JRecode <watermelo0326@gmail.com>
2015-05-13 15:03:59 +0800

    Stack overflow , PLZ

--- HW/hw3_Repo/MyCode/s1031434_function.cpp
+++ HW/hw3_Repo/MyCode/s1031434_function.cpp
@@ -62,6 +62,8 @@ Plugboard::Plugboard(string file_name){
 	if (!readfile) cout << file_name << " can't be opened!" << endl;
 	getline(readfile, data);
 	readfile.close();
+
+	timer = 0;
 	start = 'A';
 }
 
@@ -69,13 +71,21 @@ size_t Plugboard::Encoding(const size_t index){
 	if (!timer){
 		timer = 1;
 		next->spin();
-		return this->next->Encoding(next->start % 65);
+		char en_code = data[index - 65];
+		size_t pos = en_code - start;
+		return this->next->Encoding(pos);
 	}
 	if (timer){
 		timer = 0;
-		return Encoding(next->start);
-	}
-	
+		char back_code = start + index;
+		if (back_code > 90) back_code -= 26;
+		size_t location = data.find(back_code) + 65;
+		if (location > this->start)
+			location -= this->start;
+		else
+			location = this->start - location;
+		return Encoding(location + 65);
+	}	
 }
 
 /* Wheel FUNCTION */
@@ -96,17 +106,25 @@ Wheel::Wheel(string file_name,char begin,char arrow){
 size_t Wheel::Encoding(const size_t index){
 	if (!timer){
 		timer = 1;
-		char add_code = this->data[index];
-		//Distance -> add_code - pos
-		return this->next->Encoding();
+		size_t pos = this->start + index;
+		if (pos > 90) pos -= 26;
+		char add_code = data[pos - 65];
+		if (add_code > this->start)
+			pos = add_code - this->start;
+		else
+			pos = this->start - add_code;
+		return next->Encoding(pos);
 	}
 	if (timer){
 		timer = 0;
-		size_t pos = start + index;
-		char ch = pos % 91;
-		int a = data.find(ch);
-		int b = start - 65;
-		return this->previous->Encoding(abs(a - b));
+		char back_code = start + index;
+		if (back_code > 90) back_code -= 26;
+		size_t location = data.find(back_code) + 65;
+		if (location > this->start)
+			location -= this->start;
+		else
+			location = this->start - location;
+		return previous->Encoding(location);
 	}
 }
 
@@ -133,18 +151,25 @@ Special_Wheel::Special_Wheel(string file_name, char begin, char arrow){
 size_t Special_Wheel::Encoding(const size_t index){
 	if (!timer){
 		timer = 1;
-		size_t pos = (start + index) % 91;
-		char add_code = this->data[pos];
-		//Distance -> add_code - pos
-		return this->next->Encoding();
+		size_t pos = this->start + index;
+		if (pos > 90) pos -= 26;
+		char add_code = data[pos - 65];
+		if (add_code > this->start)
+			pos = add_code - this->start;
+		else
+			pos = this->start - add_code;
+		return next->Encoding(pos);
 	}
 	if (timer){
 		timer = 0;
-		size_t pos = start + index;
-		char ch = pos % 91;
-		int a = data.find(ch);
-		int b = start - 65;
-		return this->previous->Encoding(abs(a - b));
+		char back_code = start + index;
+		if (back_code > 90) back_code -= 26;
+		size_t location = data.find(back_code) + 65;
+		if (location > this->start)
+			location -= this->start;
+		else
+			location = this->start - location;
+		return previous->Encoding(location);
 	}
 }
 
@@ -167,9 +192,14 @@ Reflector::Reflector(string file_name){
 }
 
 size_t Reflector::Encoding(const size_t index){
-	char add_code = data[index];
-	size_t pos = add_code - start;
-	return this->previous->Encoding(pos);
+	size_t pos = this->start + index;
+	if (pos > 90) pos -= 26;
+	char add_code = data[pos - 65];
+	if (add_code > this->start)
+		pos = add_code - this->start;
+	else
+		pos = this->start - add_code;
+	return previous->Encoding(pos);
 }
 
 
