5183cfb3aab302eec02a6a64bb3a155a7f5f70e7
Pudding850806 <pudding850806@gmail.com>
2015-06-19 14:19:08 +0800

    modify fastpow

--- Algorithm/001.fastPow.cpp
+++ Algorithm/001.fastPow.cpp
@@ -2,9 +2,15 @@
 using namespace std ;
 /* a^n mod modBase */
 
+// O(n)
+int normalPow(int a, int n, int mod){
+	long long r = 1 ;
+	while(n--)	r = (r*a) % mod ;
+	return (int)r ;
+}
+
 // O(log(n))
-int fastPow(int a, int n, int mod){
-	long long r = 1, base = a ;
+int fastPow(int a, int n, int mod){	long long r = 1, base = a ;
 	while(n){	// n != 0
 		if(n & 1)	r = (r*base) % mod ;
 		base = (base*base) % mod ;
@@ -13,19 +19,24 @@ int fastPow(int a, int n, int mod){
 	return (int)r ;
 }
 
-// O(n)
-int normalPow(int a, int n, int mod){
-	long long r = 1 ;
-	while(n--)	r = (r*a) % mod ;
-	return (int)r ;
+// O(log(n))
+int fastPow2(int a, int n, int mod){   // a^n = (a^(n/2))^2 
+	if(n == 1)	return a % mod ;
+	long long temp = fastPow2(a, n/2, mod) % mod ;
+	if(n%2 == 1)	return ((temp * temp) % mod) * a % mod ;
+	else	return temp * temp % mod ;
 }
 
 int main(){
-	cout << fastPow(34567, 1000000000, 3333331) << endl ;
-	cout << fastPow(345678, 1000000000, 3333331) << endl ;
-	cout << fastPow(3456789, 1000000000, 3333331) << endl ;
+	cout << fastPow(34567, 2000000000, 3333331) << endl ;
+	cout << fastPow(345678, 2000000000, 3333331) << endl ;
+	cout << fastPow(3456789, 2000000000, 3333331) << endl ;
 	cout << endl ;
-	cout << normalPow(34567, 1000000000, 3333331) << endl ;
-	cout << normalPow(345678, 1000000000, 3333331) << endl ;
-	cout << normalPow(3456789, 1000000000, 3333331) << endl ;
+	cout << fastPow2(34567, 2000000000, 3333331) << endl ;
+	cout << fastPow2(345678, 2000000000, 3333331) << endl ;
+	cout << fastPow2(3456789, 2000000000, 3333331) << endl ;
+	cout << endl ;
+	cout << normalPow(34567, 2000000000, 3333331) << endl ;
+	cout << normalPow(345678, 2000000000, 3333331) << endl ;
+	cout << normalPow(3456789, 2000000000, 3333331) << endl ;
 }
