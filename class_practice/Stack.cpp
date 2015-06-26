abcbf13f4bc001d03efb31311e638f3eab5eec8d
JRecode <watermelo0326@gmail.com>
2015-06-26 20:52:36 +0800

    How to determine the function into .cpp

--- class_practice/Stack.cpp
+++ class_practice/Stack.cpp
@@ -3,101 +3,121 @@
 using namespace std;
 
 #define Maxsize 10
+template<class T>
 class Stack{
-	friend ostream& operator<<(ostream&,const Stack& R);
+	friend ostream& operator<<(ostream& output,const Stack<T>& R){
+		for(int i=0;i<R.size();i++)
+			output<<R.data[i]<<" ";
+		return output;
+	}
 public:
-	Stack();
-	Stack(const Stack&);
-	void swap(const Stack&); //C++11
-	int top()const;
-	int size()const;
-	bool empty()const;
-	void pop();
-	void push(int);
+	Stack(){
+		for(int i=0;i<Maxsize;i++)
+			this->data[i]=0;
+		length=-1;
+	}
+	Stack(const Stack<T>& R){
+		this->length=R.length;
+		for(int i=0;i<R.size();i++)
+			this->data[i]=R.data[i];
+	}
+	//void swap(const Stack&); //C++11
+	T top()const{
+		return data[length];
+	}
+	int size()const{
+		return (length+1);
+	}
+	bool empty()const{
+		return (length==-1);
+	}
+	void pop(){
+		if(empty()){
+			cout<<"Stack can't be pop"<<endl;
+		return;
+		}
+		length--;
+	}
+	void push(T x){
+		if( this->size()==Maxsize ){
+			cout<<"Stack will be overflow"<<endl;
+		return;
+		}
+		data[++length]=x;
+	}
 private:
-	int data[Maxsize];
+	T data[Maxsize];
 	int length;
-	int *ptr;		
 };
 
-ostream& operator<<(ostream& output,const Stack& R){
+//template<class T>
+/*ostream& operator<<(ostream& output,const Stack<T>& R){
 	for(int i=0;i<R.size();i++)
 		output<<R.data[i]<<" ";
 	output<<endl;
 	return output;
-}
+}*/
 
 /* Stack Function */
+/*template<class T>
 Stack::Stack(){
 	for(int i=0;i<Maxsize;i++)
 		this->data[i]=0;
 	length=-1;
-	ptr=data;
 }
 
-Stack::Stack(const Stack& R){
+template<class T>
+Stack::Stack(const Stack<T>& R){
 	this->length=R.length;
 	for(int i=0;i<R.size();i++)
 		this->data[i]=R.data[i];
-	ptr=(data+length);
 }
 
+template<class T>
 int Stack::size()const{
 	return (length+1);
 }
 
+template<class T>
 bool Stack::empty()const{
 	return (length==-1);
 }
 
+template<class T>
 void Stack::pop(){
 	if(empty()){
 		cout<<"Stack can't be pop"<<endl;
 		return;
 	}
 	length--;
-	ptr--;
 }
 
-void Stack::push(int x){
-	if( ptr == (data + Maxsize) ){
-		cout<<"Stack will be overload"<<endl;
+template<class T>
+void Stack::push(T x){
+	if( this->size()==Maxsize ){
+		cout<<"Stack will be overflow"<<endl;
 		return;
 	}
 	data[++length]=x;
-	ptr++;	
 }
 
-int Stack::top()const{
+template<class T>
+T Stack::top()const{
 	return data[length];
-}
-
-void Stack::swap(const Stack& R){
-	int arr[R.size()];
-	for(int i=0;i<R.size();i++)
-		arr[i]=R.data[i];
-	int len=R.length;
-	for(int i=0;i<this->length;i++)
-		R.data[i]=this->data[i];
-	R.length=this->length;
-	for(int i=0;i<len;i++)
-		this->data[i]=arr[i];
-	this->length=len;
-}
+}*/
 
 
 int main(){
-	Stack A;
+	Stack<int> A;
 	/* intital status */
 	cout<<"Stack is empty ? : "<<(A.empty()?"Yes":"No")<<endl;
 	cout<<"Size of Stack is : "<<A.size()<<endl;//0
 	/* add some elements */
 	for(int i=0;i<Maxsize;i++)
 		A.push(i+1);
-	cout<<endl;	
 	cout<<"Stack is empty ? : "<<(A.empty()?"Yes":"No")<<endl;
 	cout<<"Size of Stack is : "<<A.size()<<endl;
-	cout<<A;
+	cout<<A<<endl;
 	cout<<"-----Now add the 11th elements to test push-----"<<endl;
 	A.push(11);
 	int timer=A.size();
@@ -115,18 +135,15 @@ int main(){
 	
 	for(int i=0;i<Maxsize;i++)
 		A.push(i+1);
-	cout<<A;
-	Stack B(A);
-	cout<<B;
+	cout<<A<<endl;
+	Stack<int> B(A);
+	cout<<B<<endl;
 	timer=B.size();
 	for(int i=0;i<timer;i++)
 		B.pop();
 	for(int i=0;i<Maxsize;i++)
 		B.push(Maxsize-i);
-	cout<<B;
-	cout<<"----Swap A B----"<<endl;
-	A.swap(B);
-	cout<<A<<B;
+	cout<<B<<endl;
 	
 	return 0;
 }
