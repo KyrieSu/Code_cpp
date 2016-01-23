564ecd77b8b0cb8f339bc20a68ecab042c06a926
JRecode <watermelo0326@gmail.com>
2016-01-23 16:25:49 +0800

    modify map.cpp to test

--- 2016.01.20/algorithm/map.cpp
+++ 2016.01.20/algorithm/map.cpp
@@ -13,8 +13,13 @@ int main(){
 
     map<string,int> ::iterator ptr;
     ptr = m.find("Apple");
-    cout << ptr->second << endl;
+    if(ptr!=m.end())
+        cout << ptr->second << endl;
+    else
+        cout << "Can't find Apple" << endl ;
 
+    m.insert(make_pair("Banana",100));
+    cout << m["NTU"] << " HERE to get the second value " << endl;
     for(ptr = m.begin();ptr!=m.end();ptr++)
         cout << ptr->first << '\t' << ptr->second << endl;
 
