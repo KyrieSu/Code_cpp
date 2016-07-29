a2a4bc4b020505aec58003f2b601a2bb669fbe23
KyrieSu <watermelo0326@gmail.com>
2016-07-29 17:23:39 +0800

    Finish

--- 2016.07.27/Nodejs/Kyrie_callback.js
+++ 2016.07.27/Nodejs/Kyrie_callback.js
@@ -1,16 +1,20 @@
 var fs = require('fs');
 var readline = require('readline');
 
-var counter = 0;
+
 //callback(err,line)
 function countLine(filename,callback){
+    var counter = 0;
     var lineReader = readline.createInterface({
         input: fs.createReadStream(filename)
     });
-    lineReader.on('line', function(line){
+    lineReader.on('line', function(line){ // get line
         counter++;
     });
-    callback(counter);
+    lineReader.on('close',function(){ // get ^c || ^D
+        console.log('EOF');
+        callback(counter);
+    });
 };
 
 countLine('data',function(line){
