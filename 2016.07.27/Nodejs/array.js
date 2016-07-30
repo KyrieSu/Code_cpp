772cfba668a67bea1c937fc54155cb9183624a5f
KyrieSu <watermelo0326@gmail.com>
2016-07-31 00:48:24 +0800

    bug

--- 2016.07.27/Nodejs/array.js
+++ 2016.07.27/Nodejs/array.js
@@ -3,7 +3,7 @@ var readline = require('readline');
 
 var arr = ['test_hw2.ot','test_hw2_3a.ot','test_hw2_discard.ot','test_hw2_unsafe.ot']; // test data
 
-function countLine(filename,callback){
+function countLine(filename,i,callback){
     var counter = 0;
     var lineReader = readline.createInterface({
         input: fs.createReadStream(filename)
@@ -13,32 +13,31 @@ function countLine(filename,callback){
     });
     lineReader.on('close',function(){ // get ^c || ^D
         console.log('EOF');
-        callback(counter);
+        callback(i,counter);
     });
 };
 
-function finalcheck(ans,callback){
+function finialcheck(ans){
     if(ans.length!=arr.length)
-        callback('fail');
-    for(var i = 0;i<ans.length;i++)
+        return false;
+    for(var i=0;i<ans.length;i++){
         if(ans[i]==undefined)
-            callback('fail');
-    callback('success');
+            return false;
+    }
+    return true;
 }
 
 function finish(callback){
     var ans = [];
     for(var i=0;i<arr.length;i++){
-        countLine(arr[i],function(line){
-            ans[i] = line;
-            finalcheck(ans,function(msg){
-                if(msg==='success')
-                    callback(ans);
-            })
+        countLine(arr[i],i,function(index,line){
+            arr[index] = line;
+            if(finialcheck(ans))
+                callback(ans);
         });
     }
-};
+}
 
 finish(function(ans){
     console.log(ans);
-})
+});
