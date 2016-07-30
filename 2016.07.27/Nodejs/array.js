3340ed8b4d097ac5006c67a763190a83d2203f54
KyrieSu <watermelo0326@gmail.com>
2016-07-30 15:21:37 +0800

    finish callback

--- 2016.07.27/Nodejs/array.js
+++ 2016.07.27/Nodejs/array.js
@@ -2,9 +2,8 @@ var fs = require('fs');
 var readline = require('readline');
 
 var arr = ['test_hw2.ot','test_hw2_3a.ot','test_hw2_discard.ot','test_hw2_unsafe.ot']; // test data
-var ans = [];
 
-function test(filename,callback){
+function countLine(filename,callback){
     var counter = 0;
     var lineReader = readline.createInterface({
         input: fs.createReadStream(filename)
@@ -14,16 +13,21 @@ function test(filename,callback){
     });
     lineReader.on('close',function(){ // get ^c || ^D
         console.log('EOF');
-        ans.push(counter);
-        console.log(ans)
+        callback(counter);
     });
-    if(ans.length===arr.length){
-        callback('success');
+};
+
+function finish(callback){
+    var ans = [];
+    for(var i=0;i<arr.length;i++){
+        countLine(arr[i],function(line){
+            ans.push(line);
+            if(ans.length==arr.length)
+                callback(ans);
+        });
     }
 };
 
-for(var i=0;i<arr.length;i++){
-    test(arr[i],function(ans){
-        console.log(ans);
-    });
-}
+finish(function(ans){
+    console.log(ans);
+});
