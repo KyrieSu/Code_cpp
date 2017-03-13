e2efe0688e0b817b6cd2ae65486be56d5ee1e2cd
KyrieSu <watermelo0326@gmail.com>
2017-03-13 18:40:24 +0800

    to do dijkstra algorithm

--- Algorithm/Dijkstra.cpp
+++ Algorithm/Dijkstra.cpp
@@ -3,10 +3,32 @@
 #include <queue>
 using namespace std;
 
+typedef vector<vector<int> > Array2D;
+void Dijkstra(Array2D&,int);
+
 int main(){
     int N; //number of dots
     cin >> N;
-    vector<vector<int> > vec(N);
-    cout << vec.size();
+    Array2D vec(N);
+    for(int i=0;i<vec.size();i++){
+        for(int j=0;j<N;j++){
+            vec[i].push_back(0);
+        }
+    }
+    int start;
+    cout << "Enter start : " ; 
+    cin >> start;
+    int x,y,weight;
+    while(cin >> x >> y >> weight){
+        vec[x][y] = weight;
+        vec[y][x] = weight;
+    }
+    Dijkstra(vec,start);
     return 0;
+}
+
+void Dijkstra(Array2D& v,int start =0){
+    int N = v.size();
+    vector<int> dis(N,-1);
+    vector<bool> dot(N,false);
 }
