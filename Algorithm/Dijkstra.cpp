59da187149e7bb1edbd01f81476d53a7c2f9af51
KyrieSu <watermelo0326@gmail.com>
2017-03-13 15:58:10 +0800

    initial Dijkstra's algorithm

---
+++ Algorithm/Dijkstra.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+int main(){
+    int N; //number of dots
+    cin >> N;
+    vector<vector<int> > vec(N);
+    cout << vec.size();
+    return 0;
+}
