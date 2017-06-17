dc7c4dbd67e65f6ba3a216a80f20ce1c6fb2ea6d
KyrieSu <watermelo0326@gmail.com>
2017-06-17 17:01:19 +0800

    oh my god

--- CPE/2016_ICPC_A.cpp
+++ CPE/2016_ICPC_A.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <algorithm>
 #include <cmath>
 using namespace std;
 
-int gcd(int a , int b){
+int gcd(int a ,int b){
     return a ? gcd(b%a,a) : b;
 }
 
@@ -10,14 +11,28 @@ int main(){
     int r , v1 , v2 ;
     while(cin >> r >> v1 >> v2){
         if(r==0) break;
-        int v = v1-v2;
+        int v = 0;
+        if(v1>v2){
+            v = v1-v2;
+        }else{
+            v = v2-v1;
+        }
         r/=2;
         int x = gcd(r*v1,v);
-        if(x==v){
-            cout << abs(r*v1/x) << endl;
+        if(r*v1%v!=0){
+            int top = r*v1/x;
+            int btm = v/x;
+            if(v1<0){
+                top = 400*btm - top;
+            }
+            cout << abs(top) << "/" << btm << endl;
         }else{
-            cout << abs(r*v1/x) << "/" << abs(v/x) << endl;
+            int y = r*v1/v;
+            if(v1<0){
+                y -= 400-y;
+            }
+            cout << abs(y) << endl;
         }
-    } 
+    }
     return 0;
 }
